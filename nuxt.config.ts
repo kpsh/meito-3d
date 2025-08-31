5fb9758b689524ec7ff38f800eaed9a67d39885f
kpsh <kloipashaj@gmail.com>
2025-08-31 19:36:45 +0200

    feat(main): eslint setup and fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
 
   modules: [
     '@nuxt/ui',
     '@nuxt/eslint',
     '@nuxt/image',
-    '@nuxt/scripts'
+    '@nuxt/scripts',
   ],
+  ssr: false,
+  devtools: { enabled: true },
 
   css: ['~/assets/css/main.css'],
 
-  compatibilityDate: '2025-07-16'
+  compatibilityDate: '2025-07-16',
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
