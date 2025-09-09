d471e320d75ca41b74fd295ebc888459907dccca
kpsh <kloipashaj@gmail.com>
2025-09-09 20:50:54 +0200

    feat: prepare for early deployment add deploy workflow for github pages add base url and meta properties in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,19 @@ export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
 
+  app: {
+    baseURL: '/meito-3d/',
+    head: {
+      title: 'Meito 3D',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Meito 3D - Tokyo based blacksmith specializing in traditional knives and blades.' },
+        { name: 'keywords', content: 'Meito, 3D Models, 3D Generation, Blades, Knives, Swords, Traditional Craftsmanship, Japan Swords, Katana' },
+        { name: 'author', content: 'kpsh.github.io' },
+      ],
+    },
+  },
+
   css: ['~/assets/css/main.css'],
 
   compatibilityDate: '2025-07-16',
